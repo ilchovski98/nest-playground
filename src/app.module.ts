606d681313536a05387b58e9e428c4445dd8c361
ilchovski98 <ilchovski98@gmail.com>
2023-05-08 18:01:55 +0300

    Add prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, BookmarkModule, AuthModule],
+  imports: [UserModule, BookmarkModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
